b1346149cf9bad190e7685343a27bfc0ca535cbe
Oscar Eduardo Ruiz Chavez <oer000j@email.wal-mart.com>
2022-01-29 14:51:01 -0600

    CompositeMeterRegistry error por orden incorrecto de los metodos al ponerlos antes de agregar el MeterRegistry

--- src/main/java/com/devs4j/metrics/MetricsApplication.java
+++ src/main/java/com/devs4j/metrics/MetricsApplication.java
@@ -2,15 +2,21 @@ package com.devs4j.metrics;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 
 public class MetricsApplication {
 	public static void main(String[] args) {
-		MeterRegistry registry= new SimpleMeterRegistry();
 		
-		Counter counter = registry.counter("numero.empleados","oficina","Benito Juarez");
+		CompositeMeterRegistry compositeMeter = new CompositeMeterRegistry();
+		
+		Counter counter = compositeMeter.counter("numero.empleados","oficina","Benito Juarez");
 		counter.increment();
 		counter.increment(200);
+		
+		MeterRegistry registry= new SimpleMeterRegistry();
+		
+		compositeMeter.add(registry);
 		
 		System.out.printf("Numero de empleados %f", counter.count());
 	}
