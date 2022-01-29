8f26175a875e02a939778128e8a5e85dffc843e2
Oscar Eduardo Ruiz Chavez <oer000j@email.wal-mart.com>
2022-01-29 14:36:55 -0600

    Ejemplo basico de un MeterRegistry con un incremento en un counter

--- src/main/java/com/devs4j/metrics/MetricsApplication.java
+++ src/main/java/com/devs4j/metrics/MetricsApplication.java
@@ -1,7 +1,17 @@
 package com.devs4j.metrics;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+
 public class MetricsApplication {
 	public static void main(String[] args) {
+		MeterRegistry registry= new SimpleMeterRegistry();
 		
+		Counter counter = registry.counter("numero.empleados","oficina","Benito Juarez");
+		counter.increment();
+		counter.increment(200);
+		
+		System.out.printf("Numero de empleados %f", counter.count());
 	}
 }
