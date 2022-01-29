4de4fc7374ca027f721d45ad08a3dd0c41b61e35
Oscar Eduardo Ruiz Chavez <oer000j@email.wal-mart.com>
2022-01-29 15:23:58 -0600

    GlobalRegistry para poder utilizar el MeterRegistry desde cualquier lugar (lo utilizamos como un singleton)

--- src/main/java/com/devs4j/metrics/MetricsApplication.java
+++ src/main/java/com/devs4j/metrics/MetricsApplication.java
@@ -2,25 +2,35 @@ package com.devs4j.metrics;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Metrics;
 import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 
 public class MetricsApplication {
 	public static void main(String[] args) {
 		
-		CompositeMeterRegistry compositeMeter = new CompositeMeterRegistry();
+		CompositeMeterRegistry compositeMeter = Metrics.globalRegistry;
+		//System.out.println(compositeMeter==null);
 		
 		Counter counter = compositeMeter.counter("numero.empleados","oficina","Benito Juarez");
 		
 		MeterRegistry registry= new SimpleMeterRegistry();
-		MeterRegistry registry2= new SimpleMeterRegistry();
 		
 		compositeMeter.add(registry);
-		compositeMeter.add(registry2);
 		
 		counter.increment();
 		counter.increment(200);
 		
+		System.out.printf("Numero de empleados %f\n", counter.count());
+		
+		foo();
+		
+	}
+
+	private static void foo() {
+		CompositeMeterRegistry globalregistry = Metrics.globalRegistry;
+		Counter counter = globalregistry.counter("numero.empleados","oficina","Benito Juarez");
+		counter.increment(150);
 		System.out.printf("Numero de empleados %f", counter.count());
 	}
 }
