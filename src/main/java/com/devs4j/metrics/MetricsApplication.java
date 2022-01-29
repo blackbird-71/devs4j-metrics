d1296cdb8eca23c7b38413fa18fb4b75a2b1559a
Oscar Eduardo Ruiz Chavez <oer000j@email.wal-mart.com>
2022-01-29 16:13:55 -0600

    Se crea un contador (Counter) utilizando el MeterRegister y utilizando el builder del counter

--- src/main/java/com/devs4j/metrics/MetricsApplication.java
+++ src/main/java/com/devs4j/metrics/MetricsApplication.java
@@ -2,35 +2,24 @@ package com.devs4j.metrics;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.Metrics;
-import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 
 public class MetricsApplication {
 	public static void main(String[] args) {
 		
-		CompositeMeterRegistry compositeMeter = Metrics.globalRegistry;
-		//System.out.println(compositeMeter==null);
-		
-		Counter counter = compositeMeter.counter("numero.empleados","oficina","Benito Juarez");
-		
 		MeterRegistry registry= new SimpleMeterRegistry();
 		
-		compositeMeter.add(registry);
+		//Se crea el contador utilizando el MeterRegistry
+		Counter counter2 = registry.counter("devs4j.students", "course","Metricas con micrometer");
 		
+		//Se crea el contador utilizando el builder del Counter
+		Counter counter = Counter.builder("devs4j.students")
+		.description("Numero de estudiantes de devs4j")
+		.tag("course", "Metricas con micrometer")
+		.register(registry);
 		counter.increment();
 		counter.increment(200);
 		
-		System.out.printf("Numero de empleados %f\n", counter.count());
-		
-		foo();
-		
-	}
-
-	private static void foo() {
-		CompositeMeterRegistry globalregistry = Metrics.globalRegistry;
-		Counter counter = globalregistry.counter("numero.empleados","oficina","Benito Juarez");
-		counter.increment(150);
-		System.out.printf("Numero de empleados %f", counter.count());
+		System.out.println(counter.count());
 	}
 }
